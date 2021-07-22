4baa893815b5a022d1ed0e4dac6e97e95e392653
Peng.Jin <peng.jin@symbio.com>
2021-07-22 09:44:27 +0800

    ok

--- egg_server/app/extend/application.js
+++ egg_server/app/extend/application.js
@@ -1,5 +1,26 @@
+module.exports = (app) => {
+  app.once('server', (server) => {
+    // websocket
+    console.log(`server on`)
+  });
+  app.on('error', (err, ctx) => {
+    // report error
+    console.log(`error`)
+  });
+  app.on('request', (ctx) => {
+    // log receive request
+    console.log(`request`);
+  });
+  app.on('response', (ctx) => {
+    // ctx.starttime is set by framework
+    const used = Date.now() - ctx.starttime;
+    console.log(`response used`, used);
+    // log total cost
+  });
+};
+
 module.exports = {
-  foo(){
-    console.log(`this`, this.config)
-  }
-}
+  foo() {
+    console.log(`this`, this.config);
+  },
+};

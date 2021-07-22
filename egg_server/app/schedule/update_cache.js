91118475ca8d30b02855b11dfccff6b25d07d51a
Peng.Jin <peng.jin@symbio.com>
2021-07-22 17:58:04 +0800

    ok

--- egg_server/app/schedule/update_cache.js
+++ egg_server/app/schedule/update_cache.js
@@ -4,17 +4,18 @@ class UpdateCache extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      interval: '1m', // 1 分钟间隔
+      interval: '10s', // 1 分钟间隔
       type: 'all', // 指定所有的 worker 都需要执行
     };
   }
 
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    const res = await this.ctx.curl('http://www.api.com/cache', {
+    const res = await this.ctx.curl('https://api.apiopen.top/getSingleJoke?sid=28654780', {
       dataType: 'json',
     });
-    this.ctx.app.cache = res.data;
+    console.log(`res`, res)
+    this.ctx.app.cache = res.result;
   }
 }
 
